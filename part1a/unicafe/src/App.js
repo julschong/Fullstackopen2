33e75735ffdd855896824a64fff61af071977d9d
Julius Lee <71372051+julschong@users.noreply.github.com>
2021-03-27 12:30:35 -0700

    finished_1.11

--- part1a/unicafe/src/App.js
+++ part1a/unicafe/src/App.js
@@ -6,7 +6,63 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  return <div>code here</div>
+  return (
+    <div>
+      <Header />
+      <MyButton name="good" handleClick={() => setGood(good + 1)} />
+      <MyButton name="neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <MyButton name="bad" handleClick={() => setBad(bad + 1)} />
+      <Stats good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const Header = () => {
+  return <h1>give feedback</h1>
+}
+
+const MyButton = ({ name, handleClick }) => {
+  return <button onClick={handleClick}>{name}</button>
+}
+
+const Stats = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+
+  let avg = (good - bad) / total
+  let positive = good / total
+
+  if (total === 0) {
+    return (
+      <>
+        <h1>statistics</h1>
+        <h3>No feedback given</h3>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <h1>statistics</h1>
+      <MyLabel text="good" num={good} />
+      <br></br>
+      <MyLabel text="netral" num={neutral} />
+      <br></br>
+      <MyLabel text="bad" num={bad} />
+      <br></br>
+      <MyLabel text="average" num={avg} />
+      <br></br>
+      <MyLabel text="positive" num={positive} /> %
+    </>
+  )
+}
+
+const MyLabel = ({ text, num }) => {
+  return (
+    <>
+      {text} <span style={{ paddingLeft: "20px" }} />
+      {num}
+    </>
+  )
 }
 
 export default App
