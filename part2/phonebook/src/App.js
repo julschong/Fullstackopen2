c0b3187cc6ce4cbac7c4e9b57d5bdf19c53ebc49
Julius Lee <71372051+julschong@users.noreply.github.com>
2021-03-28 01:26:02 -0700

    finished-2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,19 +4,40 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }])
   const [newName, setNewName] = useState("")
 
+  const onInputNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const onSubmitAddPerson = (event) => {
+    event.preventDefault()
+    if (persons.map((person) => person.name).includes(newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    const newNameObj = { name: newName }
+    const newPersons = persons.concat(newNameObj)
+    setPersons(newPersons)
+    setNewName("")
+    event.target[0].value = ""
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={onSubmitAddPerson}>
         <div>
-          name: <input />
+          name: <input onChange={onInputNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person, i) => (
+        <p key={i}>{person.name}</p>
+      ))}
     </div>
   )
 }
