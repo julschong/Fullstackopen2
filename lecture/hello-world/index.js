e8e1ef42c927ea09b441692b2a2acd5a383ee267
Julius Lee <71372051+julschong@users.noreply.github.com>
2021-03-29 13:43:47 -0700

    finished-3a-lecture

--- lecture/hello-world/index.js
+++ lecture/hello-world/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+app.use(express.json())
 
 let notes = [
   {
@@ -29,6 +30,47 @@ app.get("/", (request, response) => {
 app.get("/api/notes", (request, response) => {
   response.json(notes)
 })
+
+app.get("/api/notes/:id", (request, response) => {
+  const note = notes.find((note) => Number(request.params.id) === note.id)
+
+  note ? response.json(note) : response.status(404).end()
+})
+
+app.delete("/api/notes/:id", (request, response) => {
+  if (notes.some((note) => note.id === Number(request.params.id))) {
+    notes = notes.filter((note) => Number(request.params.id) !== note.id)
+    response.status(204).end()
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0
+  return maxId + 1
+}
 
 const PORT = 3001
 app.listen(PORT, () => {
