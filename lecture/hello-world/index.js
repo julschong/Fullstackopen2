0940f441dc50cd8bf98d681c1f7938b009d1ab6f
Julius Lee <71372051+julschong@users.noreply.github.com>
2021-03-30 20:39:43 -0700

    save-part2

--- lecture/hello-world/index.js
+++ lecture/hello-world/index.js
@@ -1,6 +1,16 @@
 const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
 const app = express()
+app.use(cors())
 app.use(express.json())
+
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body)
+})
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+)
 
 let notes = [
   {
@@ -72,7 +82,7 @@ const generateId = () => {
   return maxId + 1
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
