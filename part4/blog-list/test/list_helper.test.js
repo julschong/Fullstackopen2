64af26fafc74e3cdf23071f146f0afa34223b482
Julius Lee <71372051+julschong@users.noreply.github.com>
2021-04-01 00:57:20 -0700

    finish-4.4

--- part4/blog-list/test/list_helper.test.js
+++ part4/blog-list/test/list_helper.test.js
@@ -3,3 +3,55 @@ const listHelper = require('../utils/list_helper')
 test('dummy returns one', () => {
     expect(listHelper.dummy()).toBe(1)
 })
+
+describe('totalLikes', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }, {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }, {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }, {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }, {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.totalLikes(Array.of(listWithOneBlog[0]))
+        expect(result).toBe(5)
+    })
+
+    test('when list has multiple blogs, equals the likes of all', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        expect(result).toBe(25)
+    })
+
+})
+
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.length ?
+        blogs.reduce((sum, current) => current.likes + sum, 0) : 0
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
