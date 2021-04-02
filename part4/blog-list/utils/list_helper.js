672ae1dd6048f2174514453257e3bea1b4de320b
Julius Lee <71372051+julschong@users.noreply.github.com>
2021-04-01 17:49:15 -0700

    Revert "finished-4.5"
    This reverts commit 2f7b9438fc0c895199bf2719ac9cbfab4ef80b19.

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,5 +1,3 @@
-const _ = require('lodash')
-
 const dummy = (blogs) => {
     return 1
 }
@@ -9,19 +7,6 @@ const totalLikes = (blogs) => {
         blogs.reduce((sum, current) => current.likes + sum, 0) : 0
 }
 
-const favoriteBlog = (blogs) => {
-    return blogs.length ?
-        blogs.reduce((fav, blog) => blog.likes > fav.likes ? fav = blog : fav) :
-        {}
-}
-
-const mostBlogs = (blogs) => {
-    let result = _.countBy(blogs, 'author')
-    let authorWithMostBlogs = Object.keys(result).reduce((a, b) => result[a] > result[b] ? a : b)
-
-    return { author: authorWithMostBlogs, blogs: result[authorWithMostBlogs] }
-}
-
 module.exports = {
-    dummy, totalLikes, favoriteBlog, mostBlogs
+    dummy, totalLikes
 }
