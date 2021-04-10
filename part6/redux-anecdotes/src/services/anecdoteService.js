bff430545bc00e53a34b4931a77f45ccc69dd2d7
Julius Lee <71372051+julschong@users.noreply.github.com>
2021-04-10 11:24:56 -0700

    finished-6.14

--- part6/redux-anecdotes/src/services/anecdoteService.js
+++ part6/redux-anecdotes/src/services/anecdoteService.js
@@ -1,11 +1,24 @@
 import axios from 'axios'
 
 const base_Url = 'http://localhost:3004/anecdotes'
+const getId = () => (100000 * Math.random()).toFixed(0)
 
 const getAll = () => {
     return axios.get(base_Url).then((res) => res.data)
 }
 
-const anecdoteService = { getAll }
+const createOne = async (content) => {
+    let res = await getAll()
+
+    const newAnecdote = {
+        content: content,
+        id: getId(),
+        vote: 0,
+    }
+    res = await axios.post(base_Url, newAnecdote)
+    return res.data
+}
+
+const anecdoteService = { getAll, createOne }
 
 export default anecdoteService
