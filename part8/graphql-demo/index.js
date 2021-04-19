e5d241f3924b1324f3535cab7fae6562eab2c390
Julius Lee <71372051+julschong@users.noreply.github.com>
2021-04-19 01:28:14 -0700

    finished-8.16

--- part8/graphql-demo/index.js
+++ part8/graphql-demo/index.js
@@ -1,9 +1,12 @@
 const path = require('path')
 require('dotenv').config({ path: path.join(__dirname, '\\.env') })
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const Book = require('./models/book')
 const Author = require('./models/author')
+const User = require('./models/user')
 
 const { ApolloServer, UserInputError, gql } = require('apollo-server')
 const _ = require('lodash')
@@ -11,7 +14,6 @@ const uuid = require('uuid')
 const book = require('./models/book')
 
 const TEST_MONGO_URI = process.env.TEST_MONGO_URI
-console.log(TEST_MONGO_URI)
 mongoose.connect(TEST_MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -31,11 +33,23 @@ const typeDefs = gql`
         id: ID!
         bookCount: Int!
     }
+
+    type User {
+        username: String!
+        favoriteGenre: String!
+        id: ID!
+    }
+
+    type Token {
+        value: String!
+    }
+
     type Query {
         allBooks(author: String, genres: String): [Book!]!
         allAuthors: [Author!]!
         bookCount: Int!
         authorCount: Int!
+        me: User
     }
     type Mutation {
         addBook(
@@ -45,6 +59,12 @@ const typeDefs = gql`
             genres: [String]
         ): Book!
         editAuthor(name: String!, setBornTo: Int!): Author
+        createUser(
+            username: String!
+            password: String!
+            favoriteGenre: String!
+        ): User
+        login(username: String!, password: String!): Token
     }
 `
 const resolvers = {
@@ -81,6 +101,9 @@ const resolvers = {
                     ).length,
                 }
             })
+        },
+        me: async (root, args, context) => {
+            return context.currentUser
         },
     },
     Mutation: {
@@ -127,12 +150,66 @@ const resolvers = {
             foundAuthor.born = args.setBornTo
             return await foundAuthor.save()
         },
+        createUser: async (root, args) => {
+            if (!(args.username && args.password && args.favoriteGenre)) {
+                throw new UserInputError(
+                    'username, password, and favorite genre cannot be empty'
+                )
+            } else if (args.username.length < 4 || args.password.length < 4) {
+                throw new UserInputError(
+                    'username and password must be at least 4 characters long'
+                )
+            }
+            const saltRound = 10
+            const passwordHash = await bcrypt.hash(args.password, saltRound)
+            const newUser = new User({
+                username: args.username.trim(),
+                password: passwordHash,
+                favoriteGenre: args.favoriteGenre,
+            })
+            return await newUser.save()
+        },
+        login: async (root, args) => {
+            if (!(args.username && args.password)) {
+                throw new UserInputError(
+                    'username and password cannot be empty'
+                )
+            }
+            const retrievedUser = await User.findOne({
+                username: args.username,
+            })
+
+            const match = await bcrypt.compare(
+                args.password,
+                retrievedUser.password
+            )
+
+            if (match) {
+                const userinfo = JSON.stringify({
+                    username: retrievedUser.username,
+                    id: retrievedUser._id,
+                    favoriteGenre: retrievedUser.favoriteGenre,
+                })
+                return { value: jwt.sign(userinfo, process.env.SECRET) }
+            }
+        },
     },
 }
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null
+        if (auth && auth.toLowerCase().startsWith('bearer ')) {
+            const decodedToken = jwt.verify(
+                auth.substring(7),
+                process.env.SECRET
+            )
+            const currentUser = await User.findById(decodedToken.id)
+            return { currentUser }
+        }
+    },
 })
 
 server.listen().then(({ url }) => {
---
+++ part8/graphql-demo/models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const schema = mongoose.Schema({
+    username: { type: String, required: true, unique: true, minlength: 4 },
+    password: { type: String, required: true, minlength: 4 },
+    favoriteGenre: { type: String, required: true },
+})
+
+module.exports = mongoose.model('User', schema)
