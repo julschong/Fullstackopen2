a1f685941a4a85beb1c28288b9f9292cae45d563
Julius <71372051+julschong@users.noreply.github.com>
2021-04-21 04:07:23 -0700

    finished-8.21

--- part8/library-frontend/src/components/Recommendation.js
+++ part8/library-frontend/src/components/Recommendation.js
@@ -1,50 +1,54 @@
-import React from 'react'
-import { useQuery } from '@apollo/client'
+import React, { useEffect } from 'react'
+import { useLazyQuery } from '@apollo/client'
 import { BOOKS_BY_GENRE } from '../graphql-requests/queries'
+import _ from 'lodash'
 
 const Recommendation = ({ userinfo }) => {
-    const result = useQuery(BOOKS_BY_GENRE, {
-        variables: { genre: userinfo.favoriteGenre || '' },
-    })
+    const [bookByGenre, { data }] = useLazyQuery(BOOKS_BY_GENRE)
 
     let books = []
 
-    if (result.loading) {
+    useEffect(() => {
+        if (!_.isEmpty(userinfo)) {
+            bookByGenre({ variables: { genre: userinfo.favoriteGenre } })
+        }
+    }, [userinfo, bookByGenre])
+
+    if (data) {
+        books = data.allBooks
+        return (
+            <div>
+                <h2>recommendations</h2>
+                <p>
+                    books in your favorite genre{' '}
+                    <strong>{userinfo.favoriteGenre}</strong>
+                </p>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>title</th>
+                            <th>author</th>
+                            <th>published</th>
+                        </tr>
+                        {books.map((a, i) => (
+                            <tr key={a.title + i}>
+                                <td>{a.title}</td>
+                                <td>{a.author.name}</td>
+                                <td>{a.published}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        )
+    } else {
         return (
             <div>
                 <h2>recommendations</h2>
                 <p>loading</p>
             </div>
         )
-    } else {
-        books = result.data.allBooks
     }
-
-    return (
-        <div>
-            <h2>recommendations</h2>
-            <p>
-                books in your favorite genre{' '}
-                <strong>{userinfo.favoriteGenre}</strong>
-            </p>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>title</th>
-                        <th>author</th>
-                        <th>published</th>
-                    </tr>
-                    {books.map((a, i) => (
-                        <tr key={a.title + i}>
-                            <td>{a.title}</td>
-                            <td>{a.author.name}</td>
-                            <td>{a.published}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    )
 }
 
 export default Recommendation
