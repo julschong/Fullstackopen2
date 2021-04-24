bdf561d3e17c19a46a2969068d632b5991755684
Julius <71372051+julschong@users.noreply.github.com>
2021-04-24 15:03:12 -0700

    finished-9.5

--- part9/exercises/calculateBMI.ts
+++ part9/exercises/calculateBMI.ts
@@ -1,4 +1,4 @@
-const calculateBMI = (height: number, weight: number): string => {
+export const calculateBMI = (height: number, weight: number): string => {
     const bmi = weight / Math.pow(height / 100, 2);
     if (bmi < 18.5) {
         return 'Underweight';
@@ -8,21 +8,23 @@ const calculateBMI = (height: number, weight: number): string => {
         return 'Overweight';
     } else if (bmi > 30) {
         return 'Obese';
+    } else {
+        return 'Error';
     }
 };
 
-if (process.argv.length < 4) {
-    throw new TypeError('Height or Weight Argument Missing!');
-}
+// if (process.argv.length < 4) {
+//     throw new TypeError('Height or Weight Argument Missing!');
+// }
 
-const height = Number(process.argv[2]);
-const weight = Number(process.argv[3]);
-if (height === 0 || weight === 0) {
-    throw new TypeError('Height and Weight cannot be zero!');
-}
+// const height = Number(process.argv[2]);
+// const weight = Number(process.argv[3]);
+// if (height === 0 || weight === 0) {
+//     throw new TypeError('Height and Weight cannot be zero!');
+// }
 
-if (!height || !weight) {
-    throw new TypeError('Height and Weight needs to be numbers!');
-}
+// if (!height || !weight) {
+//     throw new TypeError('Height and Weight needs to be numbers!');
+// }
 
-console.log(calculateBMI(height, weight));
+// console.log(calculateBMI(height, weight));
--- part9/exercises/index.ts
+++ part9/exercises/index.ts
@@ -1,9 +1,26 @@
 import express from 'express';
+import { calculateBMI } from './calculateBMI';
 
 const app = express();
 
 app.get('/hello', (_req, res) => {
     res.send('Hello FullStack');
+});
+
+app.get('/bmi', (req, res) => {
+    const height = Number(req.query.height);
+    const weight = Number(req.query.weight);
+
+    if (height && weight) {
+        const bmi = calculateBMI(height, weight);
+        res.json({
+            height,
+            weight,
+            bmi,
+        });
+    } else {
+        res.status(400).json({ error: 'params missing' });
+    }
 });
 
 const PORT = 3003;
