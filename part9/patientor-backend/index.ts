4dddea731aa80efc99f4ea62417afa7e1b6ba005
Julius Lee <7julius.lee@gmail.com>
2021-04-24 23:27:59 -0700

    finished-9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,11 +1,21 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
 const app = express();
 
-app.get('/ping', (_req, res) => {
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get('/', (_req, res) => {
+  res.send('hhello');
+});
+
+app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
 
-const PORT = 3003;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
